26afc9267213371fb4143a62187734140a21e8ea
scrabiefi <144354334+scrabiefi@users.noreply.github.com>
2024-01-04 12:23:40 +0000

    Add setup.sh that installs packages and configures ssh server

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+echo "Running setup.sh"
+echo "Installing basic packages"
+
+apk --no-cache update
+apk --no-cache add openrc sudo gcc git curl htop go python3 make nodejs postgresql-client openssh-server-pam neovim ripgrep fd lazygit bash unzip wget gzip shadow
+
+echo "Setting up ssh server"
+
+ssh-keygen -A
+mkdir /run/openrc
+touch /run/openrc/softlevel
+sed -i -E "s/#*\s*Port\s+.+/Port 2222/g" /etc/ssh/sshd_config
+sed -i 's/#*PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/#GatewayPorts no/GatewayPorts yes/g' /etc/ssh/sshd_config
+sed -i 's/#PermitTunnel no/PermitTunnel yes/g' /etc/ssh/sshd_config
+sed -i -E "s/#?\s*UsePAM\s+.+/UsePAM yes/g" /etc/ssh/sshd_config
+rc-status
+service sshd start
+
+echo "Creating a user"
+
+NEWUSER="dev"
+adduser -D $NEWUSER
+echo "$NEWUSER ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/$NEWUSER && chmod 0440 /etc/sudoers.d/$NEWUSER
