aa8a62d2e76d108d4408194ae9e4519126c49519
nTIONian <144354334+nTIONian@users.noreply.github.com>
2024-01-04 15:38:58 +0200

    Fix typo in variable name

--- .devcontainer/user-setup.sh
+++ .devcontainer/user-setup.sh
@@ -4,24 +4,24 @@ set -e
 
 echo "Running user-setup.sh"
 
-GITHUB_USERNAME="${GITHUB_USERNAME:-defaultuser}"
+GITHUB_USER="${GITHUB_USER:-defaultuser}"
 
-echo "Creating user: $GITHUB_USERNAME"
+echo "Creating user: $GITHUB_USER"
 
-adduser -D "$GITHUB_USERNAME"
-echo "$GITHUB_USERNAME ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/"$GITHUB_USERNAME" && chmod 0440 /etc/sudoers.d/"$GITHUB_USERNAME"
+adduser -D "$GITHUB_USER"
+echo "$GITHUB_USER ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/"$GITHUB_USER" && chmod 0440 /etc/sudoers.d/"$GITHUB_USER"
 
 echo "Installing lazyzim"
 
-git clone https://github.com/LazyVim/starter /home/"$GITHUB_USERNAME"/.config/nvim
-rm -rf /home/"$GITHUB_USERNAME"/.config/nvim/.git
-chown -R "$GITHUB_USERNAME":"$GITHUB_USERNAME" /home/"$GITHUB_USERNAME"/.config/nvim
+git clone https://github.com/LazyVim/starter /home/"$GITHUB_USER"/.config/nvim
+rm -rf /home/"$GITHUB_USER"/.config/nvim/.git
+chown -R "$GITHUB_USER":"$GITHUB_USER" /home/"$GITHUB_USER"/.config/nvim
 
 echo "Installing oh-my-bash"
 
 OSH_THEME="rr"
-sudo -u "$GITHUB_USERNAME" bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
-sudo -u "$GITHUB_USERNAME" sed -i "s/OSH_THEME=\".*\"/OSH_THEME=\"$OSH_THEME\"/" /home/"$GITHUB_USERNAME"/.bashrc
-sudo chsh -s "/bin/bash" "$GITHUB_USERNAME"
+sudo -u "$GITHUB_USER" bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
+sudo -u "$GITHUB_USER" sed -i "s/OSH_THEME=\".*\"/OSH_THEME=\"$OSH_THEME\"/" /home/"$GITHUB_USER"/.bashrc
+sudo chsh -s "/bin/bash" "$GITHUB_USER"
 
 echo "Done!"
